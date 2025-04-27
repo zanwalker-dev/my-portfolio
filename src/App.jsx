209212bc5677d86a6cca1418aa8dfc8980fd7d83
André "Zan" Rocha <132136080+zanwalker-dev@users.noreply.github.com>
2025-04-27 13:05:05 -0300

    set: html configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <>
       <h1>Hello</h1>
     </>
   );
-}
+};
 
 export default App;
